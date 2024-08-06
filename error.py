060ed347b5d47e4aed72a3e12a04a8f176af822e
Sutanjoy Pal <sutanjoypal04@gmail.com>
2024-08-06 10:23:32 +0530

    Add Error injectros

--- error.py
+++ error.py
@@ -1,22 +1,27 @@
 import random
 
 def inject_single_error(data):
-    data = list(data)
+    print("Injecting single error")
+    newData = list(data)
     pos = random.randint(0, len(data) - 1)
-    data[pos] = '1' if data[pos] == '0' else '0'
-    return ''.join(data)
+    newData[pos] = '1' if data[pos] == '0' else '0'
+    return ''.join(newData)
 
 def inject_double_error(data):
+    print("Injecting double error")
+    newData = list(data)
     pos1 = random.randint(0, len(data) - 1)
     pos2 = random.randint(0, len(data) - 1)
     while pos2 == pos1:
         pos2 = random.randint(0, len(data) - 1)
-    data[pos1] = '1' if data[pos1] == '0' else '0'
-    data[pos2] = '1' if data[pos2] == '0' else '0'
-    return ''.join(data)
+    newData[pos1] = '1' if data[pos1] == '0' else '0'
+    newData[pos2] = '1' if data[pos2] == '0' else '0'
+    return ''.join(newData)
 
 def inject_burst_error(data,n):
-    start = random.randint(0, len(data) - 5)
+    print(f"Injecting burst error of {n} errors")
+    newData = list(data)
+    start = random.randint(0, len(data) - 10)
     for i in range(start, start + n):
-        data[i] = '1' if data[i] == '0' else '0'
-    return ''.join(data)
+        newData[i] = '1' if data[i] == '0' else '0'
+    return ''.join(newData)
--- sender.py
+++ sender.py
@@ -4,6 +4,7 @@ import pickle
 from fileIO import read_input_file,prepare_datawords
 from checksum import calculate_checksum
 from crc import mod2div,CRC_10_POLY,CRC_16_POLY,CRC_32_POLY,CRC_8_POLY
+from error import inject_single_error,inject_double_error,inject_burst_error
 
 client=socket.socket()
 
@@ -20,7 +21,9 @@ client_address = client.getsockname()
 #STOP & WAIT to send packets
 filename = input("Enter input filename: ")
 data = read_input_file(filename)
+
 datawords = prepare_datawords(data,16)
+
 #print(calculate_checksum(data))
 datawords.append(calculate_checksum(data))
 datawords.append("-1") 
@@ -28,6 +31,14 @@ datawords.append("-1")
 appended_data = data + '0'*(len(CRC_10_POLY)-1)
 crc = mod2div(appended_data, CRC_10_POLY)
 
+datawordsList = []
+datawordsList.append(datawords)
+#datawords = prepare_datawords(inject_single_error(data),16)
+# datawordsList.append(datawords)
+#datawords = prepare_datawords(inject_double_error(data),16)
+# datawordsList.append(datawords)
+#datawords = prepare_datawords(inject_burst_error(data,20),16)
+# datawordsList.append(datawords)
 
 seq = 0
 for packet in datawords:
